fac9302a19c96bfd9c1e2b3cbf54314d416bae27
André Porto <andreporto@me.com>
2023-08-02 09:01:10 -0300

    Initial Commit

---
+++ GetMapped/GetMappedApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetMappedApp.swift
+//  GetMapped
+//
+//  Created by André Porto on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GetMappedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
